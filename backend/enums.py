f17f165ad8b668c58ddde927fbffe5aeba6880fe
alen-ky <alenkylyshbek@gmail.com>
2025-04-13 14:29:43 -0400

    Added project structure and base logic

---
+++ backend/enums.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+
+class Status(Enum):
+    Success = "Success"
+    Error = "Error"
+
+
+class ResumeUploadMessages(Enum):
+    TooLarge = (
+        "Your file size exceeds our 2 MB limit. Please try uploading a different file."
+    )
+    NotAPdf = "Invalid file type. Only PDF files are allowed."
+    ResumeUploadSuccess = "Resume uploaded successfully."
+    JobDescriptionUploadSuccess = "Job description submitted successfully."
+    JobDescriptionUploadFailure = "Job description exceeds character limit."
+
+
+class UserSignUpMessages(Enum):
+    EmailExists = "Email already exists."
+    RegisteredSuccessfully = "User registered successfully."
+
+
+class UserLogin(Enum):
+    InvalidCredentials = "Invalid email or password."
